4b9deffc52791b5f3601dd07bed95a81f1368708
Najash Marron <najashmarron@gmail.com>
2023-11-13 16:25:05 -0800

    updating the model response

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,38 @@
-const getInfoData = (): string => {
-    return "Najash3";
+const experience = [
+    {
+        id: 1,
+        company: "PayPal",
+        years: "7",
+        stack: ["asd", "aseadg", "ergser", "sdsdcg"]
+    },
+    {
+        id: 2,
+        company: "Google",
+        years: "4",
+        stack: ["asd", "aseadg", "ergser", "sdsdcg"]
+    },
+    {
+        id: 3,
+        company: "Apple",
+        years: "5",
+        stack: ["asd", "aseadg", "ergser", "sdsdcg"]
+    },
+    {
+        id: 4,
+        company: "Meta",
+        years: "9",
+        stack: ["asd", "aseadg", "ergser", "sdsdcg"]
+    },
+    {
+        id: 5,
+        company: "Netflix",
+        years: "4",
+        stack: ["asd", "aseadg", "ergser", "sdsdcg"]
+    }
+];
+
+const getInfoData = (): any[] => {
+    return experience;
 };
   
 export default getInfoData;
